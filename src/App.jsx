653a9d5af99bbf000496ac44d2ec6a0b0fcd5222
stanleychinon <chinonsostanley042@gmail.com>
2024-06-07 18:03:24 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div>
@@ -15,7 +10,7 @@ function App() {
             <a href='#'>Home</a>
           </li>
           <li>
-            <a href='#'>about</a>
+            <a href='#'>About</a>
           </li>
           <li>
             <a href='#'>Testimonial</a>
